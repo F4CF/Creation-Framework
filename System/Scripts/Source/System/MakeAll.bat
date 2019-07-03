77171cb3863913a0447402f2a52e5d69aae56535
Scrivener07 <ScrivenerMods@gmail.com>
2019-07-03 00:54:13 -0400

    Changes for XSE

---
+++ System/Scripts/Source/System/MakeAll.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+CALL "..\System\Build.bat"
+CALL "..\System.Diagnostics\Build.bat"
+CALL "..\System.Objects\Build.bat"
+CALL "..\System.Projects\Build.bat"
+CALL "..\System.XSE\Build.bat"
+CALL "..\System (Test)\Build.bat"
