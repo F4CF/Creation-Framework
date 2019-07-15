5a0253a59e699a7d4b1d0b3eba8189c9409e4f23
Scrivener07 <ScrivenerMods@gmail.com>
2019-07-14 22:39:32 -0400

    Changed build file, added an archive list for `System.esl`

--- System/Scripts/Source/System/Make.bat
+++ System/Scripts/Source/System/Make.bat
@@ -3,9 +3,3 @@ CALL "..\System\Build.bat"
 
 REM System.XSE.esl
 CALL "..\System.XSE\Build.bat"
-
-REM System.MCM.esl
-CALL "..\System.MCM\Build.bat"
-
-REM System.HUDFramework.esl
-CALL "..\System.HUDFramework\Build.bat"
